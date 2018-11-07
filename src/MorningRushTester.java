cc02419f4bf907e8bde4acc0049cdf890d58e17b
David Gilstad <david.gilstad@ou.edu>
2018-11-07 14:00:11 -0600

    Add MorningRushTester

---
+++ src/MorningRushTester.java
@@ -0,0 +1,52 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+import javax.swing.JOptionPane;
+
+public class MorningRushTester
+{   
+    private static final int DELAY = 1000;
+    static final int initialGulps = 5;
+
+    public MorningRushTester()
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        class DrinkCoffee implements ActionListener
+        {
+            private int gulps;
+            
+            public DrinkCoffee(int inGulps)
+            {
+                gulps = inGulps;
+            }
+            
+            @Override
+            public void actionPerformed(ActionEvent event)
+            {
+                if (gulps > 0)
+                {
+                    System.out.println(gulps);
+                }
+                else if (gulps == 0)
+                {
+                    System.out.println("Go to school");
+                    JOptionPane.showMessageDialog(null, "Make another");
+                    gulps = initialGulps;
+                }
+                --gulps;
+            }
+        }
+        
+        DrinkCoffee expresso = new DrinkCoffee(initialGulps);
+        Timer t = new Timer(DELAY, expresso);
+        t.start();
+        
+        JOptionPane.showMessageDialog(null, "Make another?");
+        System.exit(0);
+    }
+}
